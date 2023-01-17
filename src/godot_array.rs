d1ef82ca8b10a93991c766eb45b7ad3fb55ff557
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2023-01-17 12:48:27 +0700

    Adds more array methods

--- src/godot_array.rs
+++ src/godot_array.rs
@@ -44,6 +44,38 @@ extern "C" {
     fn array_get(ptr: u32, i: u32) -> u32;
     #[link_name = "array.set"]
     fn array_set(ptr: u32, i: u32, x: u32);
+    #[link_name = "array.remove"]
+    fn array_remove(ptr: u32, i: u32);
+    #[link_name = "array.erase"]
+    fn array_erase(ptr: u32, x: u32);
+    #[link_name = "array.resize"]
+    fn array_resize(ptr: u32, i: u32);
+    #[link_name = "array.push"]
+    fn array_push(ptr: u32, v: u32);
+    #[link_name = "array.pop"]
+    fn array_pop(ptr: u32) -> u32;
+    #[link_name = "array.push_front"]
+    fn array_push_front(ptr: u32, v: u32);
+    #[link_name = "array.pop_front"]
+    fn array_pop_front(ptr: u32) -> u32;
+    #[link_name = "array.insert"]
+    fn array_insert(ptr: u32, i: u32, x: u32);
+    #[link_name = "array.count"]
+    fn array_count(ptr: u32, x: u32) -> u32;
+    #[link_name = "array.contains"]
+    fn array_contains(ptr: u32, x: u32) -> u32;
+    #[link_name = "array.find"]
+    fn array_find(ptr: u32, x: u32, from: u32) -> i32;
+    #[link_name = "array.rfind"]
+    fn array_rfind(ptr: u32, x: u32, from: u32) -> i32;
+    #[link_name = "array.find_last"]
+    fn array_find_last(ptr: u32, x: u32) -> i32;
+    #[link_name = "array.duplicate"]
+    fn array_duplicate(ptr: u32) -> u32;
+    #[link_name = "array.clear"]
+    fn array_clear(ptr: u32);
+    #[link_name = "array.sort"]
+    fn array_sort(ptr: u32);
 }
 
 impl GodotArray {
@@ -65,9 +97,81 @@ impl GodotArray {
         unsafe { array_set(self.value.to_raw(), ix as _, v.to_raw()) }
     }
 
+    pub fn insert(&self, ix: usize, v: &GodotValue) {
+        unsafe { array_insert(self.value.to_raw(), ix as _, v.to_raw()) }
+    }
+
+    pub fn remove(&self, ix: usize) {
+        unsafe { array_remove(self.value.to_raw(), ix as _) }
+    }
+
+    pub fn erase(&self, v: &GodotValue) {
+        unsafe { array_erase(self.value.to_raw(), v.to_raw()) }
+    }
+
+    pub fn resize(&self, ix: usize) {
+        unsafe { array_resize(self.value.to_raw(), ix as _) }
+    }
+
+    pub fn push(&self, v: &GodotValue) {
+        unsafe { array_push(self.value.to_raw(), v.to_raw()) }
+    }
+
+    pub fn pop(&self) -> GodotValue {
+        unsafe { GodotValue::from_raw(array_pop(self.value.to_raw())) }
+    }
+
+    pub fn push_front(&self, v: &GodotValue) {
+        unsafe { array_push_front(self.value.to_raw(), v.to_raw()) }
+    }
+
+    pub fn pop_front(&self) -> GodotValue {
+        unsafe { GodotValue::from_raw(array_pop_front(self.value.to_raw())) }
+    }
+
+    pub fn count(&self, v: &GodotValue) -> usize {
+        unsafe { array_count(self.value.to_raw(), v.to_raw()) as _ }
+    }
+
+    pub fn contains(&self, v: &GodotValue) -> bool {
+        unsafe { array_contains(self.value.to_raw(), v.to_raw()) != 0 }
+    }
+
+    pub fn find(&self, v: &GodotValue, from: usize) -> Option<u32> {
+        let ret = unsafe { array_find(self.value.to_raw(), v.to_raw(), from as _) };
+        ret.try_into().ok()
+    }
+
+    pub fn rfind(&self, v: &GodotValue, from: usize) -> Option<u32> {
+        let ret = unsafe { array_rfind(self.value.to_raw(), v.to_raw(), from as _) };
+        ret.try_into().ok()
+    }
+
+    pub fn find_last(&self, v: &GodotValue) -> Option<u32> {
+        let ret = unsafe { array_find_last(self.value.to_raw(), v.to_raw()) };
+        ret.try_into().ok()
+    }
+
+    pub fn duplicate(&self) -> GodotArray {
+        unsafe {
+            Self {
+                value: GodotValue::from_raw(array_duplicate(self.value.to_raw())),
+            }
+        }
+    }
+
+    pub fn clear(&self) {
+        unsafe { array_clear(self.value.to_raw()) }
+    }
+
+    pub fn sort(&self) {
+        unsafe { array_sort(self.value.to_raw()) }
+    }
+
     pub fn to_vec(&self) -> Vec<GodotValue> {
-        let mut ret = Vec::new();
-        for i in 0..self.len() {
+        let l = self.len();
+        let mut ret = Vec::with_capacity(l);
+        for i in 0..l {
             ret.push(self.get(i))
         }
         ret
