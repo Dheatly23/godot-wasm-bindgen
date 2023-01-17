93cee412ac46073bfbe0624c3c39808526768664
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2023-01-17 12:49:09 +0700

    Update prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,5 @@
 pub use godot_wasm_bindgen_macro::godot_wasm_bindgen;
 
-pub use crate::godot_array::*;
-pub use crate::godot_value::*;
+pub use crate::godot_array::GodotArray;
+pub use crate::godot_value::GodotValue;
 pub use crate::primitive::*;
